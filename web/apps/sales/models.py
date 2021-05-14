c8801a47c756c82b84f5ac000526db1bb8b59a4f
Brian Ibbotson <brian@quinceleaf.dev>
2021-05-13 23:20:48 -0400

    Removed production, purchasing and sales stub apps

--- web/apps/sales/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-from apps.common import models as core_models
-
-
-class Settings(core_models.AbstractBaseModel):
-    def __str__(self):
-        return f"Settings for Sales app"
